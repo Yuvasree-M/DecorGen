c50f862ae5aefdbf5e5415ef2d88cc45a19480e8
Yuvasree-M <yuvasreemohan4sep@gmail.com>
2026-03-15 15:13:45 +0530

    Initial commit

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,9 +1,21 @@
-import admin from "firebase-admin";
-import fs from "fs";
+// import admin from "firebase-admin";
+// import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
-);
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
+// );
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// export const db = admin.firestore();
+// export const auth = admin.auth();
+
+
+import admin from "firebase-admin";
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
